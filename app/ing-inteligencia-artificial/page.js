3555a7689618999c190503a97d6b24d5281a79dc
sistemabanuelos-wq <sistemabanuelos@gmail.com>
2026-06-25 15:54:53 -0600

    Fix Zoho: esperar div#uagForma antes de inyectar script

--- app/ing-inteligencia-artificial/page.js
+++ app/ing-inteligencia-artificial/page.js
@@ -914,15 +914,29 @@ export default function IAPage() {
           <button className="btn-orange" style={{ whiteSpace:"nowrap", padding:"0.55rem 1.5rem" }}>HABLA CON UN ASESOR</button>
         </a>
       </div>
-      {/* jQuery requerido por Zoho */}
+      {/* jQuery + Zoho cargados en orden con defer */}
       <Script
         src="https://code.jquery.com/jquery-3.7.1.min.js"
         strategy="beforeInteractive"
       />
-      {/* Script Zoho */}
       <Script
-        src="https://www.uag.mx/api/formas/forma_v2?f=o2n1ydBagK&t=uagForma&campus=VIR&nivel=LI&programa=OLG26"
+        id="zoho-forma"
         strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            function loadZohoForm() {
+              if (document.getElementById('uagForma')) {
+                var s = document.createElement('script');
+                s.src = 'https://www.uag.mx/api/formas/forma_v2?f=o2n1ydBagK&t=uagForma&campus=VIR&nivel=LI&programa=OLG26';
+                s.async = false;
+                document.body.appendChild(s);
+              } else {
+                setTimeout(loadZohoForm, 300);
+              }
+            }
+            loadZohoForm();
+          `
+        }}
       />
     </>
   );
